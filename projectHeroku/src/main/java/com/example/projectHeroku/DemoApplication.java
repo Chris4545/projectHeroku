ec16365c35cec118d9b4f56efdb782e697418d86
Chris4545 <C.ung3232@gmail.com>
2021-08-18 09:30:46 -0500

    Updated demoapp.java

--- projectHeroku/src/main/java/com/example/projectHeroku/DemoApplication.java
+++ projectHeroku/src/main/java/com/example/projectHeroku/DemoApplication.java
@@ -3,11 +3,17 @@ package com.example.projectHeroku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
